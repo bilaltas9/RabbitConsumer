2854e257ac8e07ec77d22b06c82ca1bac798cb98
DESKTOP-71IV4V1\Bilal Taş <bilaltas9@gmail.com>
2022-10-31 21:25:51 +0300

    feature : initial commit fit whole solution.

---
+++ RabbitConsumer/Film.cs
@@ -0,0 +1,18 @@
+﻿namespace RabbitConsumer
+{
+    internal class Film
+    {
+        public int Id { get; set; }
+        public string? PosterPath { get; set; }
+        public bool Adult { get; set; }
+        public string? Overview { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string? OriginalTitle { get; set; }
+        public string? OriginalLanguage { get; set; }
+        public string? Title { get; set; }
+        public int VoteCount { get; set; }
+        public int VoteAverage { get; set; }
+        public int Point { get; set; }
+        public string? Note { get; set; }
+    }
+}
